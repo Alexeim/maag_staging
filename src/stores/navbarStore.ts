32b738d2431326859a607aea8bc89bbd5cdcab67
Dima-McArrow <dsm.box@gmail.com>
2026-05-06 03:22:15 +0200

    Add scroll-aware navbar layering

--- src/stores/navbarStore.ts
+++ src/stores/navbarStore.ts
@@ -1,5 +1,18 @@
 export const navbarStore = {
   isOpen: false,
+  isScrolled: false,
+  init() {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const updateScrollState = () => {
+      this.isScrolled = window.scrollY > 8;
+    };
+
+    updateScrollState();
+    window.addEventListener("scroll", updateScrollState, { passive: true });
+  },
   open() {
     this.isOpen = true;
   },
