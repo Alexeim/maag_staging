0dfa3f44665089a0e2671e17c045d98836b77bcf
Dima-McArrow <dsm.box@gmail.com>
2026-06-11 17:03:36 +0200

    revert: hybrid rendering

--- server.mjs
+++ server.mjs
@@ -1,49 +1,32 @@
-import { handler as ssrHandler } from './dist/server/entry.mjs';
-import express from 'express';
-import expressStaticGzip from 'express-static-gzip';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
+import express from "express";
+import expressStaticGzip from "express-static-gzip";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const clientDir = join(__dirname, './dist/client');
-
-app.get('/', (req, res, next) => {
-  res.sendFile(
-    join(clientDir, 'index.html'),
-    {
-      headers: {
-        'Cache-Control': 'public, max-age=0, must-revalidate',
-      },
-    },
-    (error) => {
-      if (error) {
-        next(error);
-      }
-    }
-  );
-});
 
 // Serve pre-compressed static files (.br, .gz created by astro-compressor at build time)
 app.use(
-  '/',
+  "/",
   expressStaticGzip(clientDir, {
     enableBrotli: true,
-    orderPreference: ['br', 'gz'],
+    orderPreference: ["br", "gz"],
     index: false,
     serveStatic: {
       index: false,
-      maxAge: '1y',
+      maxAge: "1y",
       immutable: true,
     },
-  })
+  }),
 );
 
 // Astro SSR handler for all dynamic routes
 app.use(ssrHandler);
 
 const port = process.env.PORT || 8080;
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || "0.0.0.0";
 
 app.listen(port, host, () => {
   console.log(`Server running on ${host}:${port}`);
